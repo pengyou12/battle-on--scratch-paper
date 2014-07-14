43206162d8bf759f30b4706bc59cab66da082b87
pengyou12 <a623917@126.com>
2014-07-14 15:13:05 +0800

    发子弹版

--- game.js
+++ game.js
@@ -72,7 +72,7 @@ function class_Bullet(sx,sy,dx,dy,id){
 		this.PosY = y;
 	}
 	this.isOut = function(){
-		if (this.PosX < 0 || this.PosX > parseInt(MapWidth) || this.PosY < 0 || this.PosY > pasrInt(MapHeight))
+		if (this.PosX < 0 || this.PosX > parseInt(MapWidth) || this.PosY < 0 || this.PosY > parseInt(MapHeight))
 			return true;
 		else
 			return false;
@@ -96,7 +96,7 @@ function class_player()
 	this.shoot = function(){
 								if(MouseClick == true)
 								{
-								MouseClick = false;
+								
 								//TO DO:add a bullut
 								}
 							};
@@ -143,8 +143,16 @@ function class_player()
 									
 							};
 }
-
-
+//判断鼠标是否被按下
+function MouseDown(ev){
+	//var e = ev || window.event;             
+    mouseposX = ev.clientX;
+    mouseposY = ev.clientY; 
+	MouseClick = true;
+}
+function MouseUp(){
+	MouseClick = false;
+}
 document.onkeydown = function (e)
 {
 	if(window.event) // IE
@@ -173,6 +181,7 @@ else if(e.which) // Netscape/Firefox/Opera
 	}
 }
 
+
 //产生0~top的伪随机数
 function RandomInteger(top){
 	return Math.round(Math.random()*top);
@@ -188,6 +197,7 @@ function Play(){
 	EnemyTotal = 0; //敌人总数
 	intervalTime = 200; //间隔时间
 	$('#playBox').append($('<div id = "enemyBox"></div>'));
+	$('#playBox').append($('<div id = "bulletBox"></div>'));
 	//设定：
 	//开始：产生 player
 	function playerGenerate(){
@@ -232,15 +242,21 @@ function Play(){
 	var bulletGenerate = function(sx,sy,dx,dy){
 		var b = new class_Bullet(sx,sy,dx,dy, BulletTotal);
 		BulletArr.push(b);
-		temp = "<div class='bullet' id='bullet"+BulletTotal+"'></div>";
+		temp = "<img style='position:absolute;' src = 'http://i2.sinaimg.cn/dy/photo/2013/images/0809/photo_zxa_0809_dot.gif'class='bullet' id='bullet"+BulletTotal+"'></div>";
 		$('#bulletBox').append(temp);
+		$('#bullet'+BulletTotal)[0].style.left = sx+"px";
+		$('#bullet'+BulletTotal)[0].style.top = sy+"px";
 		BulletTotal++;
 	}
 
 	//事件检测:
 		//每0.2秒-检测撞击：player与enemy（game over） bullet与enemey（生命值减少-检测是否死亡）
+	var checkDeath = function(){
+		
+	}
 		//每0.2秒-检测出界：player（禁止出界） enemy（消除） bullet（消除）
 		var checkRange = function(){
+			//监测人物出界
 			for(var elem in PlayerArr)
 			{
 				if (PlayerArr[elem].positionMiny > parseInt(MapHeight) ||  PlayerArr[elem].positionMaxx < 0 || PlayerArr[elem].positionMinx > parseInt(MapWidth) || PlayerArr[elem].positionMaxy < 0 )
@@ -250,12 +266,22 @@ function Play(){
 					delete PlayerArr[elem];
 					}
 			}
+			//监测怪物出界
 			for(var ele in EnemyArr)
 			{
 				if (EnemyArr[ele].isOut())
 				{	
 					$("#enemy"+ele).remove();
 					delete EnemyArr[ele];
+				}
+			}
+			//监测子弹出界
+				for(var el in BulletArr)
+			{
+				if (BulletArr[el].isOut())
+				{	
+					$("#bullet"+el).remove();
+					delete BulletArr[el];
 				}
 			}
 
@@ -278,7 +304,20 @@ function Play(){
   				$('#player0')[0].style.top = player.positionMiny + "px";
 		} 
 		setInterval(CheckKey,50);
-		//每0.1秒-检测player发射子弹：鼠标左键按下
+		//每0.05秒-检测player发射子弹：鼠标左键按下
+		var CheckMouse = function(){
+			if(MouseClick)
+			{	
+				// cursor.PosX = mouseposX;
+				// cursor.PosY = mouseposY;
+				var DX = mouseposX - player.positionMinx;
+				var DY = mouseposY - player.positionMiny;
+				DX = DX /Math.sqrt((DX*DX)+(DY*DY));
+				DY = DY /Math.sqrt((DX*DX)+(DY*DY));
+				bulletGenerate(player.positionMinx, player.positionMiny, DX, DY);
+			}
+		}
+		var ReCheckMouse = setInterval(CheckMouse,00);
 	//事件进行：
 		//每0.2秒-子弹直线运动
 		var bulletMove = function(){
@@ -287,10 +326,10 @@ function Play(){
 				var newX = cursor.PosX + cursor.DirectX * cursor.moveLength;
 				var newY = cursor.PosY + cursor.DirectY * cursor.moveLength;
 				BulletArr[i].setPos(newX, newY);
-				$('.bullet')[i].animate({left:""+newX+"px", top:""+newY+"px"}, 'fast');
+				$("#bullet"+i).animate({left:""+newX+"px", top:""+newY+"px"}, 10);
 			}
 		}
-		ReBulletMove = setInterval(bulletMove, intervalTime);
+		var ReBulletMove = setInterval(bulletMove, intervalTime);
 
 		//每0.2秒-enemy（1）（3）直线运动
 		var enemyMoveStraight = function(){
@@ -305,7 +344,7 @@ function Play(){
 				}
 			}
 		}
-		ReEnemyMoveStraight = setInterval(enemyMoveStraight, intervalTime);
+		var ReEnemyMoveStraight = setInterval(enemyMoveStraight, intervalTime);
 
 		//每0.2秒-enemy（2）瞄准player运动
 		var enemyMoveToward = function(){
@@ -339,6 +378,7 @@ function Play(){
 		ReEnemyShootToward = setInterval(enemyShootToward, intervalTime*2);
 }
 
-var LeftArrow,RightArrow,UpArrow,DownArrow;//记录按键情况
+var LeftArrow,RightArrow,UpArrow,DownArrow, MouseClick;//记录按键情况
 var MapWidth,MapHeight;
+var mouseposX,mouseposY;
 Play();
